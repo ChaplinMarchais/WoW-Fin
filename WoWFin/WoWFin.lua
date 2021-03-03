0f433cad807cc34d1b00c04945aa2237b55cc730
Chaplin Marchais <35165054+ChaplinMarchais@users.noreply.github.com>
2021-03-03 21:33:28 +0000

    Creation of the initial files required by the addon system. Began to implement the addon hosts code which manages the primary lifetime methods for the host. Also created an External directory where all of the external lua libraries are stored for use.

---
+++ WoWFin/WoWFin.lua
@@ -0,0 +1,21 @@
+local _ ,
+WoWFin = LibStub("AceAddon-3.0"):NewAddon("WoWFin", "AceConsole-3.0")
+
+print("hello world!")
+
+-- ==============================================================================================
+-- Addon Host Lifetime
+-- ==============================================================================================
+
+function WoWFin:OnInitialize()
+
+end
+
+function WoWFin:OnEnable()
+    
+end
+
+function WoWFin:OnDisable()
+    
+end
+
