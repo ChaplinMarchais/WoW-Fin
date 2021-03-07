6a08a9fd8a262e3ce3b96382a7250e48ce26ab83
Chaplin Marchais <35165054+ChaplinMarchais@users.noreply.github.com>
2021-03-07 05:40:17 +0000

    Modified Title to FinWoW and added Ace3.0
    Added the full suite of Ace3.0 Addon libararies to the folder structure.

--- FinWoW/core.lua
+++ FinWoW/core.lua
@@ -1,14 +1,12 @@
 local _ ,
-WoWFin = LibStub("AceAddon-3.0"):NewAddon("WoWFin", "AceConsole-3.0")
-
-print("hello world!")
+FinWoW = LibStub("AceAddon-3.0"):NewAddon("FinWoW-Host", "AceConsole-3.0", "AceEvent-3.0")
 
 -- ==============================================================================================
 -- Addon Host Lifetime
 -- ==============================================================================================
 
 function WoWFin:OnInitialize()
-
+    self:RegisterEvent("AUCTION_HOUSE_SHOW")
 end
 
 function WoWFin:OnEnable()
@@ -19,3 +17,6 @@ function WoWFin:OnDisable()
     
 end
 
+function FinWoW:AUCTION_HOUSE_SHOW()
+    self:Print("You have opened the AH!")
+end
